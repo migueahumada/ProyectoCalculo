9f481b25cf973a1027a0a191eb7e2173707c69e8
migueahumada <migangahuga@gmail.com>
2026-05-10 15:19:28 -0600

    Struct Player

--- inc/Player.h
+++ inc/Player.h
@@ -1,25 +1,37 @@
 #pragma once
 #include "raymath.h"
+#include "raylib.h"
 
 typedef struct Player
 {
-  Vector2 position;
-  Vector2 velocity;
-  Vector2 acceleration;
+  Vector2 m_position;
+  Vector2 m_rotation;
+  Vector2 m_velocity;
+  Vector2 m_acceleration;
+  Texture m_texture;
 } Player;
+
+void InitPlayer(Player* player,
+                Vector2 position,
+                Vector2 velocity,
+                Vector2 acceleration, 
+                const char* texturePath)
+{
+  
+}
 
 void InitPlayerPosition(Player* player, float x, float y)
 {
-  player->position.x = x;
-  player->position.y = y;
+  player->m_position.x = x;
+  player->m_position.y = y;
 }
 
 void PlayerSetPosition(Player* player, Vector2 newPosition)
 {
-  player->position = newPosition;
+  player->m_position = newPosition;
 }
 
 Vector2 PlayerGetPosition(Player* player, Vector2 newPosition)
 {
-  return player->position;
+  return player->m_position;
 }
